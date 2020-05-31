92970b1730cea1fcd12b4b6807dfa6bcac33fd22
Nikita Nikolov <nnikolov-post@gmail.com>
2020-05-31 07:05:38 +0100

    Removed bg image

--- src/components/Jumbotron/index.jsx
+++ src/components/Jumbotron/index.jsx
@@ -8,18 +8,28 @@ import logoImage from '../../assets/images/logo.svg'
 
 const Jumbotron = ({image, children, overlay, full}) => {
 	const headerClass = cx(
-		'fixed md:relative w-full left-0 top-0 min-h-auto bg-cover bg-center pb-2 md:pb-8 z-10',
+		'fixed md:relative w-full left-0 top-0 min-h-auto bg-cover pb-2 md:pb-8 z-10 bg-none bg-primary',
 		{
-			'md:min-h-screen bg-fixed': full,
+			'md:min-h-screen': full,
 			'md:min-h-half': !full,
+		},
+	)
+
+	const bgClass = cx(
+		'absolute w-full h-full bg-cover bg-center invisible md:visible',
+		{
+			'bg-fixed': full,
 		},
 	)
 
 	return (
 		<header
 			className={headerClass}
-			style={{backgroundImage: `url(${image})`}}
 		>
+			<div
+				style={{backgroundImage: `url(${image})`}}
+				className={bgClass}
+			/>
 			{overlay && (<div className="absolute w-full h-full bg-primary bg-opacity-75" />)}
 			<div className="md:hidden absolute w-full h-full flex items-center justify-center">
 				<img
